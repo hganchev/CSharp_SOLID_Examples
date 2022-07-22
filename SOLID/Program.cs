49c1b894e3032bfe54a9c2e4e36ecc8099b6d417
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2022-07-22 13:11:56 +0300

    Open Close Principle example

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace SOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mySRP = new SRP();
+            var myOCP = new OCP();
+        }
+    }
+}
